6b53f51c5783a022c208c4a1f09bbc90c6bf11c7
Caio <eu@randomixs.com>
2021-11-03 22:39:22 -0300

    Auto-reconnect

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
+var arr = [];
 $.getJSON('servers.json', (data) => {
 	if(data) {
-		function e(server) {$('.server.box[server-id="' + server['id'] + '"] .server.latency').text('? ms').css('background','#ffb2b4')}
-		data.forEach((server) => {
+		data.forEach((server, i) => {
 			if(server['cc'] != "BR") return;
 			$('.servers.container').append('<div class="server box" server-id="' + server['id'] + '"><div class="server name">' + server['sponsor'].replace('-','') + '</div><div class="server location">' + server['name'] + '</div><div class="server latency">0 ms</div></div>');
-            var connection = new WebSocket('wss://' + server['host'] + '/ws');
-			var start, ok = false;
-			connection.onopen = function () {
-				setInterval(function () {
-					if(ok == true) return;
-					ok = true;
-					start = Date.now();
-					connection.send('PING ' + start);
-				}, 500);
-			};
-			connection.onerror = function () {e(server)};
-			connection.onclose = function () {e(server)};
-			connection.onmessage = function (e) {
-				if(e['data'].match('PONG')) {
-					var latency = (Date.now()) - start;
-					$('.server.box[server-id="' + server['id'] + '"] .server.latency').text(latency + ' ms').css('background',(latency > 100 ? '#ffb2b4' : (latency > 50 ? '#ffe8bd':'#fff')));
-					ok = false;
-				}
+			arr[i] = function () {
+				var connection = new WebSocket('wss://' + server['host'] + '/ws');
+				var start, ok = false;
+				connection.onopen = function () {
+					setInterval(function () {
+						if(ok == true) return;
+						ok = true;
+						start = Date.now();
+						connection.send('PING ' + start);
+					}, 500);
+				};
+				connection.onerror = function () {$('.server.box[server-id="' + server['id'] + '"] .server.latency').text('? ms').css('background','#ffb2b4');arr[i]()};
+				connection.onclose = function () {$('.server.box[server-id="' + server['id'] + '"] .server.latency').text('? ms').css('background','#ffb2b4');arr[i]()};
+				connection.onmessage = function (e) {
+					if(e['data'].match('PONG')) {
+						var latency = (Date.now()) - start;
+						$('.server.box[server-id="' + server['id'] + '"] .server.latency').text(latency + ' ms').css('background',(latency > 100 ? '#ffb2b4' : (latency > 50 ? '#ffe8bd':'#fff')));
+						ok = false;
+					}
+				};
 			};
 		});
+		for (i = 0; i < arr.length; i++) {
+			arr[i]();
+		};
 	} else {
 		alert('Erro ao carregar a lista de servidores.');
 	}
